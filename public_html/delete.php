158e93d58c608a0278c66132a28f68f64c545637
Tom Clegg <tomc@tomc.freelogy.org>
2009-12-16 11:35:12 -0500

    implement "delete"

---
+++ public_html/delete.php
@@ -0,0 +1,32 @@
+<?php
+
+include "lib/setup.php";
+
+$response = array();
+
+$item_id = evidence_get_latest_edit ($_POST[v], $_POST[a], $_POST[g]);
+if ($item_id) {
+  $q = theDb()->query ("INSERT INTO edits SET
+			edit_oid=?, edit_timestamp=NOW(),
+			is_draft=1, is_delete=1,
+			previous_edit_id=?,
+			variant_id=?, article_pmid=?, genome_id=?",
+		  array (getCurrentUser("oid"),
+			 $item_id,
+			 $_POST[v], $_POST[a], $_POST[g]));
+  if (theDb()->isError($q))
+    $response["errors"][] = $q->getMessage();
+  else if (($delete_id = theDb()->getOne ("SELECT LAST_INSERT_ID()"))) {
+    evidence_submit ($delete_id);
+    $response["deleted"] = true;
+  }
+}
+else {
+  $response["errors"][] = "Nothing to delete";
+}
+
+header ("Content-type: application/json");
+print json_encode ($response);
+
+?>
+
