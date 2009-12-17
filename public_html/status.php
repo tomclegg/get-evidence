df0de0316cf1ded1fdfef9806d7e1732d24dde4a
Tom Clegg <tomc@tomc.freelogy.org>
2009-12-17 18:20:05 -0500

    add "status" page

---
+++ public_html/status.php
@@ -0,0 +1,27 @@
+<?php
+
+include "lib/setup.php";
+$gOut["title"] = "Evidence Base: Status";
+$gOut["content"] = $gTheTextile->textileThis (<<<EOF
+h1. Next steps
+
+* make variant summary editable
+* fix "PMIDs missing when not logged in"
+* add "download latest db"
+* make variant_dominance and variant_impact visible/editable
+* update "genome evidence" using json data from snp.med
+* get PMID summary from pubmed when adding and show first author in PMID section heading?
+* use blosum score
+* show frequency data
+* handle edit conflicts
+
+h1. Recent steps
+
+* add "status" page
+
+EOF
+);
+
+go();
+
+?>
