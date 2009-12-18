03fe2efaf70342b44fcd7eacc47f9724623e44c8
Tom Clegg <tomc@tomc.freelogy.org>
2009-12-17 21:46:45 -0500

    more todo

--- public_html/status.php
+++ public_html/status.php
@@ -12,10 +12,11 @@ In expected approximate chronological order:
 * deploy at evidence.personalgenomes.org
 * add EB data source to trait-o-matic
 * update "genome evidence" using json data from snp.med
-* get PMID summary from pubmed when adding and show first author in PMID section heading?
+* get PMID summary from pubmed when adding and show author/title in PMID section heading
 * use blosum score
 * show frequency data
 * handle edit conflicts
+* prevent "you have unsaved changes" from scrolling off the top on long pages
 
 h1. Recent steps
 
