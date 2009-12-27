4a3afa872ad23adfc47750e97aeb0eab1cd44e5c
Tom Clegg <tomc@tomc.freelogy.org>
2009-12-27 16:03:04 -0500

    add "editors" page

---
+++ public_html/editors.php
@@ -0,0 +1,39 @@
+<?php
+
+include "lib/setup.php";
+
+$report_title = "Contributors";
+$gOut["title"] = "Evidence Base: $report_title";
+
+function print_content($x)
+{
+  global $report_title;
+  print "<h1>$report_title</h1>\n\n";
+
+  $q = theDb()->query ("SELECT edit_oid, count(*) edit_count, UNIX_TIMESTAMP(MAX(edit_timestamp)) t, eb_users.* FROM edits
+	LEFT JOIN eb_users ON edit_oid=oid
+	WHERE is_draft=0
+	GROUP BY edit_oid
+	ORDER BY edit_timestamp DESC");
+  if (theDb()->isError ($q)) die ($q->getMessage());
+
+  print "<UL>";
+  while ($row =& $q->fetchRow()) {
+    print "<LI>";
+
+    print ("<A href=\"edits?oid=".urlencode($row["edit_oid"])."\">".
+	   (htmlspecialchars ($row["fullname"] ? $row["fullname"] : $row["nickname"])).
+	   "</A>");
+
+    print " -- ".$row["edit_count"]." edits";
+
+    print strftime (" -- latest %b %e", $row["t"]);
+
+    print "</LI>\n";
+  }
+  print "</UL>\n";
+}
+
+go();
+
+?>
