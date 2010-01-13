bc7b656e43ffedd1b788686962d4d485d2f0d43b
Tom Clegg <tomc@tomc.freelogy.org>
2010-01-13 03:50:46 -0500

    include more fields in database dump: dbsnp id, overall frequency (currently just 1000-genomes), #genomes, #web hits

--- public_html/download.php
+++ public_html/download.php
@@ -10,11 +10,22 @@ else if ($_GET["version"] == "latest" || ereg ("/latest", $_SERVER["PATH_INFO"])
 
 if ($snap) {
 
-  $q = theDb()->query ("SELECT * FROM variants v
+  $q = theDb()->query ("SELECT v.*, s.*,
+			if(vo.rsid,concat('rs',vo.rsid),null) dbsnp_id,
+			COUNT(vo.dataset_id) genome_hits,
+			y.hitcount web_hits,
+			vf.num overall_frequency_n,
+			vf.denom overall_frequency_d,
+			vf.f overall_frequency
+			FROM variants v
 			LEFT JOIN snap_$snap s ON s.variant_id=v.variant_id
+			LEFT JOIN variant_frequency vf ON v.variant_id=vf.variant_id
+			LEFT JOIN variant_occurs vo ON v.variant_id=vo.variant_id
+			LEFT JOIN yahoo_boss_cache y ON v.variant_id=y.variant_id
 			WHERE s.variant_id IS NOT NULL
 			AND NOT (article_pmid>0)
-			AND NOT (genome_id>0)");
+			AND NOT (genome_id>0)
+			GROUP BY v.variant_id");
   if (theDb()->isError($q)) {
     header ("HTTP/1.1 500 Internal server error");
     die ("Database error: " . $q->getMessage());
@@ -26,6 +37,12 @@ if ($snap) {
 		      "variant_aa_change",
 		      "variant_dominance",
 		      "variant_impact",
+		      "dbsnp_id",
+		      "overall_frequency_n",
+		      "overall_frequency_d",
+		      "overall_frequency",
+		      "genome_hits",
+		      "web_hits",
 		      "summary_short");
   print ereg_replace ("\tvariant_", "\t",
 		      ereg_replace ("variant_dominance", "variant_inheritance",
