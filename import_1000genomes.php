a78128ab9eaefb46492f1310e152a6b89f0e8ad3
Tom Clegg <tomc@tomc.freelogy.org>
2010-01-13 01:30:18 -0500

    finish up 1000-genomes import

--- import_1000genomes.php
+++ import_1000genomes.php
@@ -26,6 +26,7 @@ theDb()->query ("CREATE TABLE import_1000genomes (
  chr_pos INT UNSIGNED NOT NULL,
  allele CHAR(1) NOT NULL,
  occur INT UNSIGNED NOT NULL,
+ denom INT UNSIGNED NOT NULL,
  UNIQUE(chr,chr_pos,allele)
  )");
 
@@ -46,10 +47,11 @@ for ($i=1; $i<$_SERVER["argc"]; $i++) {
 	    $bytes_read += strlen ($line);
 	    list ($chr, $pos, $alleles) = explode ("\t", trim ($line));
 	    $alleles = strtoupper ($alleles);
+	    $tot = strlen ($alleles);
 	    foreach (array ("A", "C", "G", "T") as $base) {
 		$n = substr_count ($alleles, $base);
 		if ($n > 0)
-		    fputs ($fh, "chr$chr\t$pos\t$base\t$n\n");
+		    fputs ($fh, "chr$chr\t$pos\t$base\t$n\t$tot\n");
 	    }
 	    if (++$line_count % 100000 == 0)
 		printf ("\010\010\010\010\010\010\010\010\010\010\010\010\010%10d...", $line_count);
@@ -67,8 +69,7 @@ for ($i=1; $i<$_SERVER["argc"]; $i++) {
 	die ("fork failed, giving up.\n");
     }
     reconnectDb();
-    $q = theDb()->query ("DROP TABLE import_1000genomes_onefile");
-    $q = theDb()->query ("CREATE TABLE import_1000genomes_onefile LIKE import_1000genomes");
+    $q = theDb()->query ("CREATE TEMPORARY TABLE import_1000genomes_onefile LIKE import_1000genomes");
     $q = theDb()->query ("LOAD DATA LOCAL INFILE ?
 	 INTO TABLE import_1000genomes_onefile
 	 FIELDS TERMINATED BY '\t'
@@ -79,8 +80,20 @@ for ($i=1; $i<$_SERVER["argc"]; $i++) {
     print "\n";
 
     print "Merging...";
-    reconnectDb();
-    $q = theDb()->query ("INSERT INTO import_1000genomes SELECT * FROM import_1000genomes_onefile ON DUPLICATE KEY UPDATE import_1000genomes.occur=import_1000genomes.occur+VALUES(occur)");
+    $q = theDb()->query ("INSERT INTO import_1000genomes
+ SELECT import_1000genomes_onefile.*
+ FROM import_1000genomes_onefile
+ LEFT JOIN variant_occurs vo
+	 ON import_1000genomes_onefile.chr=vo.chr
+	 AND import_1000genomes_onefile.chr_pos=vo.chr_pos
+	 AND import_1000genomes_onefile.allele=vo.allele
+ WHERE vo.variant_id IS NOT NULL
+ GROUP BY import_1000genomes_onefile.chr,
+	import_1000genomes_onefile.chr_pos,
+	import_1000genomes_onefile.allele
+ ON DUPLICATE KEY UPDATE
+	 import_1000genomes.occur=import_1000genomes.occur+VALUES(occur),
+	 import_1000genomes.denom=import_1000genomes.denom+VALUES(denom)");
     if (theDb()->isError($q)) print $q->getMessage();
     print theDb()->affectedRows();
     print "\n";
@@ -88,10 +101,17 @@ for ($i=1; $i<$_SERVER["argc"]; $i++) {
     $q = theDb()->query ("DROP TEMPORARY TABLE import_1000genomes_onefile");
 }
 
-/*
-print "Looking up chr:pos in variant_occurs and assigning variant_ids...";
+
+print "Copying data into allele_frequency table...";
+theDb()->query ("REPLACE INTO allele_frequency
+ (dbtag, chr, chr_pos, allele, num, denom)
+ SELECT ?, chr, chr_pos, allele, occur, denom
+ FROM import_1000genomes",
+		array ("1000g"));
 print theDb()->affectedRows();
 print "\n";
-*/
+
+
+theDb()->query ("DROP TEMPORARY TABLE variant_frequency_tmp");
 
 ?>
