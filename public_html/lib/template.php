b3bf413d4bfd861ffeb96e065eef457a403cc960
Tom Clegg <tomc@tomc.freelogy.org>
2010-01-18 20:28:33 -0500

    set browser encoding to utf8

--- public_html/lib/template.php
+++ public_html/lib/template.php
@@ -23,12 +23,14 @@ function frag($tag)
      }
 }
 
+header('Content-Type: text/html; charset=UTF-8');
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
 
 <head>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 <meta name="description" content="description"/>
 <meta name="keywords" content="keywords"/> 
 <meta name="author" content="author"/> 
