b9b93db1d557229347cca5b303c9d89fa6425d46
Madeleine Ball <mad@printf.net>
2010-04-07 12:52:35 -0400

    Adding changes to about and default.css

--- public_html/about.php
+++ public_html/about.php
@@ -3,35 +3,60 @@
 include "lib/setup.php";
 $gOut["title"] = "Evidence Base: About";
 $gOut["content"] = $gTheTextile->textileThis (<<<EOF
-h1. Overview
+h1. GET-Evidence
 
-Evidence Base is a public database of human genome variations of **clinical interest**.
+GET-Evidence is a system for analyzing genetic variants. All aspects of GET-Evidence are subject to improvement&mdash;we hope that others will contribute to the design of the system as well as to individual variant evaluations. Please see our "guide to editing GET-evidence":guide_editing to learn how to participate.
 
-h1. Data
+h2. Variants
 
-Variants are specified by gene, amino acid coordinate, and AA change (for example, "NPHP4 R848W").
+Currently GET-evidence only tracks nonsynonymous SNPs (missense and nonsense mutations). The automatic portion of genome analysis takes all reported SNPs and finds all nonsynonymous SNPs (missense and nonsense mutations)—these are then loaded into GET-evidence's database of variants. There variants are specified by gene name followed by amino acid change, for example "NPHP4 R848W":NPHP4-R848W (or NPHP4 Arg848Trp) refers to an amino acid change of arginine to tryptophan at position 848 in the gene product of NPHP4. 
 
-Each variant is stored with:
+h3. Variant Impact Score
 
-* a description of the variant's clinical relevance;
-* a list of publications (with Pub Med IDs) that support the description;
-* a summary of how each publication supports the description;
-* a list of publicly available individual genomes exhibiting the variant;
-* an assessment of how each individual genome supports the description (if available).
+Each variant is graded in six categories, the first four related to evidence and last two related to clinical importance. Each category may be assigned up to five points to reflect the strength of evidence in that category. Zero points reflects no significant evidence in a category, -1 reflects contradicting evidence. These six categories are the "variant impact score":guide_impact_score.
+* Computational evidence
+* Functional evidence
+* Case/control evidence
+* Familial evidence
+* Disease severity
+* Disease treatability
 
-h1. Access
+Variant impact is classified into one of four categories:
+* pathogenic
+* benign
+* protective
+* pharmacogenetic
 
-The data is free to obtain and use, either by means of the web service or by downloading a copy of the database.
+The number of points in the variant impact score categories is used to assign "qualifiers":guide_qualifiers that reflect variant evidence and clinical importance. A pathogenic variant with high clinical importance but low evidence is called "high clinical importance, unknown pathogenic".
 
-h1. Contributing
+h3. Resources for variant evaluation
 
-Everyone may contribute.
+Various databases and tools are included within GET-evidence to assist variant evaluation. These include:
 
-All contributions are listed in the "latest" database snapshot as soon as they are submitted.
+* "Online Mendelian Inheritance in Man":http://www.ncbi.nlm.nih.gov/omim (OMIM): Any variants mentioned in OMIM are noted by GET-evidence.
+* "GeneTests":http://www.genetests.org: A list of genes for which genetic testing exists.
+* BLOSUM100 is used as a score to reflect how nonconservative an amino acid change is.
+* "HapMap":http://hapmap.ncbi.nlm.nih.gov/ and "1000 Genomes":http://www.1000genomes.org: allele frequency information from these projects is recorded.
 
-Contributions are then considered by curators for inclusion in the "release" database snapshot.
+Some of these internal data sources are used to automatically generate "preliminary variant impact scores":guide_preliminary_score. This focuses attention on variants with potentially clinical relevance; later evaluation of the variants replaces the preliminary scoring.
 
-All submissions must be available for free public distribution under a CC0 license.
+Relevant publications may be added to a variant's page using that publication's PubMed ID. GET-evidence then generates an entry for that publication on the variant's page with the paper's authors and title. A field is provided below this in which an evaluator can fill out a summary describing the paper's relevance to this variant.
+
+h2. Genomes
+
+GET-Evidence also provides a location for analyzing whole genome data&mdash;currently all publically available genomes are loaded into GET-Evidence. Each variant lists the genomes within which the variant was found.
+
+You can also access individual genomes at "snp.med.harvard.edu":http://snp.med.harvard.edu. If you go to "snp.med.harvard.edu/authenticate":http://snp.med.harvard.edu/authenticate and use the username "getevidence" and the password "demo" you will have access to links to GET-evidence pages from the genome pages.
+
+h2. Access
+
+The data within GET-evidence is free to obtain and use, either by means of the web service or by downloading a copy of the database.
+
+h2. Contributing
+
+We encourage users of GET-Evidence to add, correct, and update variant evaluations, please see our "editing guide":guide_editing to learn how. Contributions are immediately reflected in the downloadable database. All contributions to GET-evidence's variant evaluations must be available for free public distribution under a CC0 license.
+
+If you wish to extend and improve upon the GET-Evidence software, we welcome additional contributors to the project. Please contact us for instructions. General suggestions on other potential improvements are also welcomed.
 EOF
 );
 
