4d4f939f6291834bd6b2980704ca1bc0177b30c6
Madeleine Ball <meprice@gmail.com>
2010-09-11 16:02:03 -0400

    Changing tabs from 2 spaces to 4 spaces

--- public_html/genome_upload.php
+++ public_html/genome_upload.php
@@ -9,25 +9,25 @@ include('xmlrpc/xmlrpc.inc');
 
 // Сheck that we have a file
 if((!empty($_FILES["genotype"])) && ($_FILES['genotype']['error'] == 0)) {
-  $filename = basename($_FILES['genotype']['name']);
-  $ext = substr($filename, strrpos($filename, '.') + 1);
-  if (($ext == "txt" || $ext == "gff") && ($_FILES["genotype"]["size"] < 300000000)) {
-    $tempname = $_FILES['genotype']['tmp_name'];
-    $shasum = sha1_file($tempname);
-    $page_content .= "shasum is $shasum<br>";
-    $permname = "/tmp/$shasum/genotype.gff";
-    // Attempt to move the uploaded file to its new place
-    mkdir ("/tmp/$shasum");
-    if (move_uploaded_file($tempname, $permname)) {
-      $page_content .= "It's done! The file has been saved as: $permname<br>"; 
+    $filename = basename($_FILES['genotype']['name']);
+    $ext = substr($filename, strrpos($filename, '.') + 1);
+    if (($ext == "txt" || $ext == "gff") && ($_FILES["genotype"]["size"] < 300000000)) {
+        $tempname = $_FILES['genotype']['tmp_name'];
+        $shasum = sha1_file($tempname);
+        $page_content .= "shasum is $shasum<br>";
+        $permname = "/tmp/$shasum/genotype.gff";
+        // Attempt to move the uploaded file to its new place
+        mkdir ("/tmp/$shasum");
+        if (move_uploaded_file($tempname, $permname)) {
+            $page_content .= "It's done! The file has been saved as: $permname<br>"; 
+        } else {
+            $page_content .= "Error: A problem occurred during file upload!";
+        }
     } else {
-      $page_content .= "Error: A problem occurred during file upload!";
+        $page_content .= "Error: Only .txt or .gff files under 1MB are accepted for upload";
     }
-  } else {
-     $page_content .= "Error: Only .txt or .gff files under 1MB are accepted for upload";
-  }
 } else {
-  $page_content .= "Error: No file uploaded";
+    $page_content .= "Error: No file uploaded";
 }
 
 // Now, send the filename to the xml-rpc server.
