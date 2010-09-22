e1cb050e429663c1c1c2989239f9018ed1675ca7
Madeleine Ball <meprice@gmail.com>
2010-09-22 17:04:33 -0400

    Add export and scrub of eb_users table
    Adding export of the users table, which includes oid, name, and email.
    Because we don't want to be sharing email addresses, we want to scrub
    this exported table so the name is retained but the email is hidden,
    so this column is scrubbed from the dump using command line perl.

--- dump_database.php
+++ dump_database.php
@@ -23,4 +23,18 @@ $user = escapeshellarg($regs[1]);
 $pass = escapeshellarg($regs[2]);
 $host = escapeshellarg($regs[3]);
 $db = escapeshellarg($regs[4]);
-passthru ("mysqldump -e -u $user -p$pass -h $host $db allele_frequency articles datasets diseases edits flat_summary gene_disease genomes snap_latest variant_disease variant_external variant_frequency variant_locations variant_occurs variants | gzip -9v > $dumpfile.tmp && mv $dumpfile.tmp $dumpfile && ls -l $dumpfile");
+
+# Explanation of command-line perl below that strips the email from eb_users:
+# 
+
+passthru ("mysqldump -e -u $user -p$pass -h $host $db allele_frequency articles datasets diseases eb_users edits flat_summary gene_disease genomes snap_latest variant_disease variant_external variant_frequency variant_locations variant_occurs variants | perl -ne '
+if (/^INSERT INTO `eb_users` VALUES /) { 
+  if (s/^INSERT INTO \`eb_users` VALUES \(([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)\);/INSERT INTO \`eb_users` VALUES \($1,$2,$3,NULL,$5\);/) { 
+    print; 
+  } else {
+    # skip output of lines that match first but not second regex--columns may have changed, do not want to accidentally fail to scrub emails 
+  } 
+} else { 
+  print; }' | gzip -9v > $dumpfile.tmp && mv $dumpfile.tmp $dumpfile && ls -l $dumpfile");
+
+
