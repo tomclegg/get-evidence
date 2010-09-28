fb28860f5c4b4d83ff7dfca8f3b67efa2425a128
Madeleine Ball <meprice@gmail.com>
2010-09-28 15:33:27 -0400

    Abstraction of dbSNP file reading

--- server/gff_dbsnp_query_from_file.py
+++ server/gff_dbsnp_query_from_file.py
@@ -15,27 +15,55 @@ from config import DBSNP_SORTED
 dbSNP_file = open(os.getenv('DATA') + "/" + DBSNP_SORTED)
 dbSNP_data = dbSNP_file.readline().split()
 
+class dbSNP:
+    def __init__(self, filename):
+        self.f = open(filename)
+        self.data = self.f.readline().split()
+        self.position = (self.data[1], int(self.data[2]))
+
+    def up_to_position(self, position):
+        while (self.data and self.comp_position(self.position, position) < 0):
+            self.data = self.f.readline().split()
+            self.position = (self.data[1], int(self.data[2]))
+        return self.position
+
+    def comp_position(self, position1, position2):
+        # positions are tuples of chromosome (str) and position (int)
+        if (position1[0] != position2[0]):
+            return cmp(position1[0],position2[0])
+        else:
+            return cmp(position1[1],position2[1])
+
+    def data(self):
+        return self.data
+
+    def position(self):
+        return self.position
+            
+
 def main():
     # return if we don't have the correct arguments
     if len(sys.argv) < 2:
         raise SystemExit(__doc__.replace("%prog", sys.argv[0]))
 
-    # now read the file and loop through
-    genome_file = gff.input(sys.argv[1])
-    dbSNP_file = open(os.getenv('DATA') + "/" + DBSNP_SORTED)
-    dbSNP_data = dbSNP_file.readline().split()
+    # Set up dbSNP input
+    dbSNP_input = dbSNP(os.getenv('DATA') + "/" + DBSNP_SORTED)
 
+    # Loop through genome file
+    genome_file = gff.input(sys.argv[1])
     for record in genome_file:
-        if record.seqname.startswith("chr"):
+        # chromosome prefix not used by dbSNP, so it is removed if present
+        if record.seqname.startswith("chr") or record.seqname.startswith("Chr"):
             chromosome = record.seqname[3:]
         else:
             chromosome = record.seqname
 
-        while (dbSNP_data and (dbSNP_data[1] < chromosome or \
-                (dbSNP_data[1] == chromosome and int(dbSNP_data[2]) < record.start - 1))):
-            dbSNP_data = dbSNP_file.readline().split()
+        # position is adjusted to match the zero-start used by dbSNP positions
+        record_position = (chromosome, record.start - 1)
+
+        dbSNP_position = dbSNP_input.up_to_position(record_position)
             
-        if (dbSNP_data and int(dbSNP_data[2]) == record.start - 1):
+        if (dbSNP_input.comp_position(dbSNP_position,record_position) == 0):
             if record.version >= 3:
                 record.attributes["Dbxref"] = "dbsnp:rs%s" % dbSNP_data[0]
             else:
