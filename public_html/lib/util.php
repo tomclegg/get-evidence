676e8e44b53738bd25ae903f76f3dc439637166f
Tom Clegg <tom@tomclegg.net>
2011-02-23 18:28:12 -0500

    add "download nsSNP and dbSNP report"

--- public_html/lib/util.php
+++ public_html/lib/util.php
@@ -14,4 +14,12 @@ function arrays_partially_equal (&$a, &$b, &$relevant_fields)
     return TRUE;
 }
 
+function humanreadable_size ($bytes)
+{
+    if ($bytes >= 2000000000) return floor($bytes / 1000000000) . " GB";
+    if ($bytes >= 2000000) return floor($bytes / 1000000) . " MB";
+    if ($bytes >= 2000) return floor($bytes / 1000) . " KB";
+    return $bytes . " B";
+}
+
 ?>
