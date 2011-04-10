8d1ef9202a31a5508adb92168987983a9a3338b9
Madeleine Price Ball <meprice@gmail.com>
2011-04-10 18:55:42 -0400

    tweak genome_analyzer, store input type in metadata

--- server/genome_analyzer.py
+++ server/genome_analyzer.py
@@ -34,6 +34,7 @@ from conversion import cgivar_to_gff, gff_from_23andme
 from utils import autozip
 
 SCRIPT_DIR = os.path.dirname(sys.argv[0])
+DEFAULT_BUILD = 'b36'
 
 def detect_format(f_in):
     """Take str generator input, determine genome file format (GFF or CGIVAR)"""
@@ -96,23 +97,23 @@ def detect_format(f_in):
     print "Giving up after 100 lines... Can't figure out data format."
     return "UNKNOWN"
 
-def process_source(genome_in):
+def process_source(genome_in, metadata=dict()):
     """
     Take source, uncompress, sort, and convert to GFF as needed, yield GFF
     """
     # Handle genome compression, get input, and make best guess of format type.
     source_input = autozip.file_open(genome_in, 'r')
-    in_type = detect_format(source_input)
+    metadata['input_type'] = detect_format(source_input)
 
     # Reset input and convert to GFF if necessary.
     source_input.close()
     source_input = autozip.file_open(genome_in, 'r')
 
-    if in_type == "GFF":
+    if metadata['input_type'] == "GFF":
         gff_input = source_input
-    elif in_type == "CGIVAR":
+    elif metadata['input_type'] == "CGIVAR":
         gff_input = cgivar_to_gff.convert(source_input)
-    elif in_type == "23ANDME":
+    elif metadata['input_type'] == "23ANDME":
         gff_input = gff_from_23andme.convert(source_input)
     else:
         print "ERROR: genome file format not recognized"
@@ -123,7 +124,7 @@ def process_source(genome_in):
     sort_cmd = ['sort', '--buffer-size=20%', '--key=1,1', '--key=4n,4']
     sort_out = subprocess.Popen(sort_cmd, stdin=subprocess.PIPE, 
                                 stdout=subprocess.PIPE, bufsize=1)
-    genome_build = "b36"
+    genome_build = DEFAULT_BUILD
     b36_list = ["hg18", "36", "b36", "build36", "NCBI36"]
     b37_list = ["hg19", "37", "b37", "build37", "GRCh37"]
     for line in gff_input:
@@ -195,6 +196,7 @@ def genome_analyzer(genotype_file, server=None):
     args = { 'genotype_input': str(genotype_file),
              'miss_out': os.path.join(output_dir, 'missing_coding.json'),
              'sorted_out': os.path.join(output_dir, 'source_sorted.gff.gz'),
+             'nonsyn_out_tmp': os.path.join(output_dir, 'ns_tmp.gff.gz'),
              'nonsyn_out': os.path.join(output_dir, 'ns.gff.gz'),
              'getev_out': os.path.join(output_dir, 'get-evidence.json'),
              'metadata_out': os.path.join(output_dir, 'metadata.json'),
@@ -212,17 +214,21 @@ def genome_analyzer(genotype_file, server=None):
 
     # Process and sort input genome data
     log.put ('#status 0/100 converting and sorting input file')
-    gff_in_gen = process_source(args['genotype_input'])
-    genome_data = { 'build': gff_in_gen.next() }
+    genome_data = dict()
+    gff_in_gen = process_source(args['genotype_input'], genome_data)
+
+    # We pass it as a yield (instead of in metadata) to force the generator to 
+    # read the header portion of the input data (which checks for build info).
+    genome_data['genome_build'] = gff_in_gen.next()
 
     # Set up build-dependent file locations
-    if (genome_data['build'] == "b36"):
+    if (genome_data['genome_build'] == "b36"):
         args['dbsnp'] = os.path.join(os.getenv('DATA'), DBSNP_B36_SORTED)
         args['reference'] = os.path.join(os.getenv('DATA'), 
                                          REFERENCE_GENOME_HG18)
         args['transcripts'] = os.path.join(os.getenv('DATA'), 
                                            KNOWNGENE_HG18_SORTED)
-    elif (genome_data['build'] == "b37"):
+    elif (genome_data['genome_build'] == "b37"):
         args['dbsnp'] = os.path.join(os.getenv('DATA'), DBSNP_B37_SORTED)
         args['reference'] = os.path.join(os.getenv('DATA'), 
                                          REFERENCE_GENOME_HG19)
@@ -235,10 +241,10 @@ def genome_analyzer(genotype_file, server=None):
     chrlist = ['chr' + str(x) for x in range(1, 22) + ['X', 'Y']]
 
     # Process genome through a series of GFF-formatted string generators.
-    log.put('#status 4 looking up reference alleles and '
+    log.put('#status 20 looking up reference alleles and '
             'dbSNP IDs, computing nonsynonymous changes, '
             'cross-referencing GET-Evidence database')
-    progtrack = ProgressTracker(sys.stderr, [5, 99], expected=chrlist, 
+    progtrack = ProgressTracker(sys.stderr, [22, 99], expected=chrlist, 
                                 metadata=genome_data)
     # Record chromosomes seen and genome coverage.
     metadata_gen = get_metadata.genome_metadata(gff_in_gen,
@@ -265,11 +271,12 @@ def genome_analyzer(genotype_file, server=None):
                                        progresstracker=progtrack)
 
     # Printing to output, pulls data through the generator chain.
-    ns_out = autozip.file_open(args['nonsyn_out'], 'w')
+    ns_out = autozip.file_open(args['nonsyn_out_tmp'], 'w')
     for line in nonsyn_gen2:
         ns_out.write(line + "\n")
     ns_out.close()
     os.system("mv " + args['getev_out'] + ".tmp " + args['getev_out'])
+    os.system("mv " + args['nonsyn_out_tmp'] + " " + args['nonsyn_out'])
 
     # Print metadata
     metadata_f_out = open(args['metadata_out'], 'w')
