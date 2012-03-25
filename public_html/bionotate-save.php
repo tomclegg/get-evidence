92aeb54ac79779172b2447273522cb97455dda60
Tom Clegg <tom@clinicalfuture.com>
2012-03-24 21:46:16 -0400

    pass xml to and from bionotate to avoid edit skew

--- public_html/bionotate-save.php
+++ public_html/bionotate-save.php
@@ -5,21 +5,25 @@
 
 include "lib/setup.php";
 
-$variant_name = evidence_get_variant_name($_REQUEST['variant_id'], '-', true);
-$bionotate_key = $_REQUEST['article_pmid'] . '-' . $variant_name;
-$html_or_xml = file_get_contents ('http://bionotate.biotektools.org/GET-Evidence/retrieve/'.$bionotate_key.'?oid='.urlencode(getCurrentUser('oid')));
-if (!preg_match ('{SNIPPET_XML = "(.*)";?\r?\n}', $html_or_xml, $regs) &&
-    !preg_match ('{^(<\?xml .*)}is', $html_or_xml, $regs))
-  exit ("No snippet found at .../$bionotate_key");
-$xml = preg_replace('{>\\r?\\n\\s*}', '>', $regs[1]);
+$variant_name = evidence_get_variant_name($_GET['variant_id'], '-', true);
+$bionotate_key = $_GET['article_pmid'] . '-' . $variant_name;
+if (isset($_POST['xml']))
+  $xml = $_POST['xml'];
+else {
+  $html_or_xml = file_get_contents ('http://bionotate.biotektools.org/GET-Evidence/retrieve/'.$bionotate_key.'?oid='.urlencode(getCurrentUser('oid')));
+  if (!preg_match ('{SNIPPET_XML = "(.*)";?\r?\n}', $html_or_xml, $regs) &&
+      !preg_match ('{^(<\?xml .*)}is', $html_or_xml, $regs))
+    exit ("No snippet found at .../$bionotate_key");
+  $xml = preg_replace('{>\\r?\\n\\s*}', '>', $regs[1]);
+}
 
 $snap_row = theDb()->getRow ("SELECT * FROM snap_latest WHERE variant_id=? AND article_pmid=? AND genome_id=0 AND disease_id=0 LIMIT 1",
-                         array ($_REQUEST['variant_id'], $_REQUEST['article_pmid']));
+                         array ($_GET['variant_id'], $_GET['article_pmid']));
 if (theDb()->isError($snap_row))
   exit($edit->getMessage());
 
 $edit = theDb()->getRow ("SELECT * FROM edits WHERE variant_id=? AND article_pmid=? AND genome_id=0 AND disease_id=0 AND edit_oid=? AND previous_edit_id=? ORDER BY edit_id DESC LIMIT 1",
-			 array ($_REQUEST['variant_id'], $_REQUEST['article_pmid'], getCurrentUser('oid'), $snap_row['edit_id']));
+			 array ($_GET['variant_id'], $_GET['article_pmid'], getCurrentUser('oid'), $snap_row['edit_id']));
 
 if (theDb()->isError($edit))
   exit($edit->getMessage());
@@ -55,4 +59,4 @@ else {
     exit("edit_id $edit_id does not make sense");
   }
 }
-header("Location: /{$variant_name}#a".$_REQUEST['article_pmid']);
+header("Location: /{$variant_name}#a".$_GET['article_pmid']);
--- public_html/lib/evidence.php
+++ public_html/lib/evidence.php
@@ -715,8 +715,11 @@ function evidence_get_report ($snap, $variant_id)
           ) {
         if (!isset($GLOBALS['bionotate_earliest']))
           $GLOBALS['bionotate_earliest'] = time();
-        else if (time() - $GLOBALS['bionotate_earliest'] > 3)
-          // don't spend more than 3 seconds getting new bionotate abstracts
+        else if (time() - $GLOBALS['bionotate_earliest'] > 4)
+          // If we've already waited more than 4 seconds for new
+          // bionotate abstracts while fulfilling request, just use
+          // what we've already received and get the rest next time
+          // around.
           break;
         if ($v[0]['variant_gene'])
           $bionotate_key = htmlentities($row['article_pmid']."-".
@@ -727,9 +730,9 @@ function evidence_get_report ($snap, $variant_id)
         else
           $bionotate_key = htmlentities($row['article_pmid']."-rs".
                                         $v[0]['variant_rsid']);
-        $ctx = stream_context_create(array('http'=>array('timeout'=>2)));
+        $ctx = stream_context_create(array('http'=>array('timeout'=>8)));
         if (preg_match ('{SNIPPET_XML = "(.*?)";?\r?\n}s',
-                        $html = @file_get_contents ('http://bionotate.biotektools.org/GET-Evidence/retrieve/'.$bionotate_key, 0, $ctx),
+                        $html = @file_get_contents ('http://bionotate.biotektools.org/GET-Evidence/pmid/'.$bionotate_key, 0, $ctx),
                         $regs) ||
             preg_match ('{^(<\?xml .*)}is', $html, $regs)) {
           $xml = preg_replace('{>\\r?\\n\\s*}', '>', $regs[1]);
